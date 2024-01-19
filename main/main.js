4579ea869abc042df4daff4e41d1e277ad578f31
Edward Huynh <edwardleehuynh@gmail.com>
2024-01-19 21:08:40 +0800

    read, review, i think htis is poggies

--- main/main.js
+++ main/main.js
@@ -1,3 +1,9 @@
+currentPath = '';
+const readData = localStorage.getItem('read');
+readFilePaths = readData ? JSON.parse(readData) : [];
+const reviewData = localStorage.getItem('review');
+reviewFilePaths = reviewData ? JSON.parse(reviewData) : [];
+
 async function readJSONFile() {
   try {
     const response = await fetch('output.json');
@@ -12,7 +18,6 @@ async function readJSONFile() {
 }
 
 readJSONFile().then((jsonData) => {
-  console.log(jsonData);
   const fileSystemDisplay = document.getElementById('filetree');
   const fileTreeElement = createFileTreeElement(jsonData);
   fileSystemDisplay.appendChild(fileTreeElement);
@@ -22,9 +27,16 @@ readJSONFile().then((jsonData) => {
 function createFileTreeElement(node, depth = 0) {
   const listItem = document.createElement('li');
   listItem.textContent = node.name;
-  listItem.style.marginLeft = `${depth * 20}px`;
+  listItem.style.marginLeft = `${depth * 10}px`;
+  if (readFilePaths.includes(node.path)){
+    listItem.style.color = "#808080";
+  }
+  if (reviewFilePaths.includes(node.path)){
+    listItem.style.color = "#004fb3";
+  }
   listItem.addEventListener('click', () => {
     loadFile(node.path);
+      
     event.stopPropagation();
   });
 
@@ -41,8 +53,94 @@ function createFileTreeElement(node, depth = 0) {
 }
 
 function loadFile(path) {
+  if (readFilePaths.includes(path)){
+    document.getElementById("readbox").checked = true;
+  } else {
+    document.getElementById("readbox").checked = false;
+  }
+  if (reviewFilePaths.includes(path)){
+    document.getElementById("reviewbox").checked = true;
+  } else {
+    document.getElementById("reviewbox").checked = false;
+  }
+
   const absoluteURL = window.location.origin + '/' + path;
   //window.location.href = absoluteURL;
-  document.getElementById("viewer").children[0].src = `https://docs.google.com/gview?url=${absoluteURL}&embedded=true`;
-  document.getElementById("fileh2").innerHTML = path.replace(".pdf", "").replace(".PDF", "").replace(".doc", "").replace(".docx", "")
+  if (/\.(pdf|docx?|txt)/i.test(path)) {
+    document.getElementById("viewer").children[0].src = `https://docs.google.com/gview?url=${absoluteURL}&embedded=true`.replace("http://127.0.0.1:8080/", "https://demeter.toomwn.xyz/"); //for live-server, obviously google cant access something being served locally, so this assumes it exists on the website
+    document.getElementById("fileh2").innerHTML = path.split('/').pop().replace(".pdf", "").replace(".PDF", "").replace(".doc", "").replace(".docx", "").replace(".txt", "");
+    currentPath = path;
+  }
+  else {
+    document.getElementById("viewer").children[0].src = ``;
+  }
+}
+
+function reloadTree() {
+  readJSONFile().then((jsonData) => {
+    const fileSystemDisplay = document.getElementById('filetree');
+    fileSystemDisplay.removeChild(fileSystemDisplay.children[0]);
+    const fileTreeElement = createFileTreeElement(jsonData);
+    fileSystemDisplay.appendChild(fileTreeElement);
+  });
+}
+
+function markRead() {
+  if (document.getElementById("readbox").checked) {
+    readFilePaths.push(currentPath);
+    localStorage.setItem('read', JSON.stringify(readFilePaths));
+    reloadTree(); 
+  } else {
+    removeFromRead();
+    localStorage.setItem('read', JSON.stringify(readFilePaths));
+    reloadTree(); 
+  }
+}
+
+function markReview() {
+  if (document.getElementById("reviewbox").checked) {
+    reviewFilePaths.push(currentPath);
+    localStorage.setItem('review', JSON.stringify(reviewFilePaths));
+    reloadTree(); 
+  } else {
+    removeFromReview();
+    localStorage.setItem('review', JSON.stringify(reviewFilePaths));
+    reloadTree(); 
+  }
+}
+
+function removeFromRead() {
+  if (readFilePaths.indexOf(currentPath)!==-1) {
+    readFilePaths.splice(readFilePaths.indexOf(currentPath), 1);
+    removeFromRead();
+  }
+}
+
+function removeFromReview() {
+  if (reviewFilePaths.indexOf(currentPath)!==-1) {
+    reviewFilePaths.splice(reviewFilePaths.indexOf(currentPath), 1);
+    removeFromReview();
+  }
+}
+
+function clearRead() {
+  if(confirm("Are you sure?\nThis will irreversibly remove any memory of any files marked as read in the local storage.\n\nPress OK to confirm.")){
+    readFilePaths = [];
+    localStorage.setItem('read', JSON.stringify(readFilePaths));
+    reloadTree(); 
+  } 
+  document.getElementById("readclearbox").checked = false;
+  document.getElementById("readbox").checked = false;
+
+}
+
+function clearReview() {
+  if(confirm("Are you sure?\nThis will irreversibly remove any memory of any files marked for review in the local storage.\n\nPress OK to confirm.")){
+    reviewFilePaths = [];
+    localStorage.setItem('review', JSON.stringify(reviewFilePaths));
+    reloadTree(); 
+  } 
+  document.getElementById("reviewclearbox").checked = false;
+  document.getElementById("reviewbox").checked = false;
+
 }
